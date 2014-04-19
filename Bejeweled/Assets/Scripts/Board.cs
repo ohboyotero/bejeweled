f175b830f5e322508c9bb44a94f4c3e300e570d2
Daniel Otero <ohboyotero@gmail.com>
2014-04-18 23:25:33 -0700

    Added a Gem script and a Board script and generated a board's worth of poop.

---
+++ Bejeweled/Assets/Scripts/Board.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+
+  public Gem gemPrefab;
+  public Sprite[] gemSprites;
+  public int rows;
+  public int columns;
+
+  private float spacing;
+
+  private Gem[,] gems;
+
+  void Start() {
+    gems = new Gem[rows, columns];
+    spacing = -transform.position.x / 4;
+    FillRandomAssShit();
+  }
+
+  void Update() {
+
+  }
+
+  private void FillRandomAssShit() {
+    Vector3 upperLeft = transform.position;
+
+    for (int row = 0; row < rows; ++row) {
+      for (int column = 0; column < columns; ++column) {
+        Vector3 position = upperLeft;
+        position.x += column * spacing + (spacing / 2);
+        position.y -= row * spacing + (spacing / 2);
+        gems[row, column] = (Gem)Instantiate(gemPrefab, position, Quaternion.identity);
+      }
+    }
+  }
+}
---
+++ Bejeweled/Assets/Scripts/Gem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gem : MonoBehaviour {
+
+  public enum Type {
+    GREEN,
+    PURPLE,
+    RED,
+    ORANGE,
+    YELLOW,
+    BLUE
+  }
+
+  public Type type;
+
+  // Use this for initialization
+  void Start() {
+  
+  }
+  
+  // Update is called once per frame
+  void Update() {
+  
+  }
+}
