a789dc44a2f78a6e06339d9cb70a8057967152bd
Daniel Otero <ohboyotero@gmail.com>
2014-07-06 01:14:42 -0700

    Added a button rollover script to switch sprites based on mouse interaction.

---
+++ Bejeweled/Assets/Scripts/ButtonRollover.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class ButtonRollover : MonoBehaviour {
+
+  public Sprite upSprite;
+  public Sprite hoverSprite;
+  public Sprite downSprite;
+
+  private SpriteRenderer spriteRenderer;
+  private bool hover;
+  private bool down;
+
+  void Start() {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+    hover = false;
+    down = false;
+  }
+	
+  void OnMouseDown() {
+    down = true;
+    spriteRenderer.sprite = downSprite;
+  }
+
+  void OnMouseUp() {
+    down = false;
+    if (hover) {
+      spriteRenderer.sprite = hoverSprite;
+    } else {
+      spriteRenderer.sprite = upSprite;
+    }
+  }
+
+  void OnMouseEnter() {
+    hover = true;
+    if (!down) {
+      spriteRenderer.sprite = hoverSprite;
+    }
+  }
+
+  void OnMouseExit() {
+    hover = false;
+    spriteRenderer.sprite = upSprite;
+  }
+}
