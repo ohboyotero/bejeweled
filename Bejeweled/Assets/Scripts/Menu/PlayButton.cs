fc00831c39d62a99e52f7894ce8aa4bf4c3b5cfc
Daniel Otero <ohboyotero@gmail.com>
2014-07-06 01:41:40 -0700

    Added a new MainMenu scene which can trigger the Game scene, and added both scenes to the project's build settings.

---
+++ Bejeweled/Assets/Scripts/Menu/PlayButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class PlayButton : MonoBehaviour {
+
+  private Vector3 downCoord;
+
+  // The maximum number of pixels that the mouse can move between mousedown and mouseup and still
+  // be considered a click.
+  private const float MAX_MOUSE_DRIFT = 3;
+
+  void OnMouseDown() {
+    downCoord = Input.mousePosition;
+  }
+
+  void OnMouseUp() {
+    if ((Input.mousePosition - downCoord).magnitude < MAX_MOUSE_DRIFT) {
+      LoadGameScene();
+    }
+  }
+
+  private void LoadGameScene() {
+    Application.LoadLevel("Game");
+  }
+}
